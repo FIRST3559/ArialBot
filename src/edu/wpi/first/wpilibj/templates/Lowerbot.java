ed92540c960a4645e842d5bbd5d1cb3b36a34f56
robot <robot@10.1.51.216>
2014-01-25 15:28:12 -0500

    DB 1/25 V1.1 Drive system added, beta testing N/A, IDK if works

--- src/edu/wpi/first/wpilibj/templates/Lowerbot.java
+++ src/edu/wpi/first/wpilibj/templates/Lowerbot.java
@@ -6,13 +6,14 @@ import edu.wpi.first.wpilibj.SimpleRobot;
 
 public class Lowerbot extends SimpleRobot {
     //RobotDrive drive = new RobotDrive (1,2);// Drive  
-    Joystick LeftStick = new Joystick (1);  // Left drive stick
-    Joystick RightStick = new Joystick (2); // Right drive stick
+    Joystick leftStick = new Joystick (1);  // Left drive stick
+    Joystick rightStick = new Joystick (2); // Right drive stick
     Joystick ThirdStick = new Joystick (3); // Arm joystick
     Compressor mainCompressor = new Compressor(1,1); //Compressor Object
     Solenoid piston1out = new Solenoid(1); //First Solenoid Object
     Solenoid piston1in = new Solenoid (2); //Second Solenoid Object
     Victor testMotor = new Victor(1);
+    RobotDrive mainDrive = new RobotDrive(1,2);
     /*
     public void robotinit(){
         mainCompressor.start();
@@ -34,16 +35,21 @@ public class Lowerbot extends SimpleRobot {
         }
     }
     
+    public void driving(){
+        mainDrive.tankDrive(-(leftStick.getY(GenericHID.Hand.kLeft)),
+                        -(rightStick.getY(GenericHID.Hand.kLeft)));   
+    }
+    
     public void operatorControl() {
         while (true && isOperatorControl() && isEnabled()){
    
         
         
-    if(LeftStick.getRawButton(6) == true){
+    if(leftStick.getRawButton(6) == true){
         piston1out.set(true);
         piston1in.set(false);
     }
-    else if(LeftStick.getRawButton(7) == true){
+    else if(leftStick.getRawButton(7) == true){
         piston1in.set(true);
         piston1out.set(false);
     }
